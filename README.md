0ed1768d5e98422f18865cd6ca21c38d40a35f68
Joshua Koo <zz85nus@gmail.com>
2018-01-21 09:32:52 +0800

    Experiment 005 - visualize keyboard

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 Midi Visualization Experiments
 
 
+Experiment 005
+
+- Visualize Keyboard
+
 Experiment 004 - 21 Jan 2018
 
-Use of [soundfont.js](https://github.com/danigb/soundfont-player) for audio samples
+- Use of [soundfont.js](https://github.com/danigb/soundfont-player) for audio samples
--- keyboard.js
+++ keyboard.js
@@ -1,27 +1,5 @@
 // reused and modified from http://www.blurspline.com/labs/harpsichord/harpsichord2.html
 
-var keyWidth = 25;
-var keyHeight = 150;
-var blacKeyWidth = 20;
-var blackKeyHeight = 100;
-
-var octaves = 8;
-
-var keyboard = document.createElement('canvas');
-document.body.appendChild(keyboard);
-
-keyboard.width = 1600;
-keyboard.height = 200;
-
-var ctx = keyboard.getContext('2d');
-
-ctx.save();
-// ctx.scale(0.8, 0.8);
-// ctx.translate(10, 10);
-
-var blacks = [ 1, 3,     6, 8, 10];
-var whites = [0, 2, 4, 5, 7, 9, 11];
-var white_offsets = [0, 1, 3, 4, 5];
 
 /*
 1 << 1 | 1 << 3 | 1 << 6 | 1 << 8 | 1 << 10
@@ -35,38 +13,18 @@ function isBlack(pos) {
     return (1 << pos & 1354) !== 0
 }
 
-var piano_keys = {};
-
-
 
 // Middle C = 60
 // Low A = 21
 // Low C = 24
 
+var keyWidth = 25;
+var keyHeight = 150;
+var blacKeyWidth = 20;
+var blackKeyHeight = 100;
 
-function repaint() {
-    ctx.clearRect(0, 0, keyboard.width, keyboard.height);
-    
-    for (var octave = 0; octave < octaves; octave++) {
-        // draw white
-        for (var key = 0; key < 7; key++) {
-            var note = octave * 12 + whites[key];
-            var played = piano_keys[note];
-            
-            drawWhiteNote(octave, key, played)
-        }
 
-        // draw black
-        for (var key = 0; key < 5; key++) {
-            var note = octave * 12 + blacks[key];
-            var played = piano_keys[note];
-            
-            drawBlackNote(octave, white_offsets[key], played)
-        }
-    }
-}
-
-function drawWhiteNote(octave, i, played) {
+function drawWhiteNote(ctx, octave, i, played) {
     if (played) {
         ctx.fillStyle = 'red';
     }  else {
@@ -76,7 +34,7 @@ function drawWhiteNote(octave, i, played) {
     ctx.fillRect( (octave * 7 + i) * keyWidth, 0, keyWidth, keyHeight);
 }
 
-function drawBlackNote(octave, i, played) {
+function drawBlackNote(ctx, octave, i, played) {
     if (played) {
         ctx.fillStyle = 'red';
     }  else {
@@ -88,17 +46,65 @@ function drawBlackNote(octave, i, played) {
     ctx.fillRect( (octave * 7 + i) * keyWidth + off, 0, blacKeyWidth, blackKeyHeight);
 }
 
-function toggleNote(note, value) {
-    if (value === undefined) {
-        piano_keys[note] = !piano_keys[note];
-    }
-    else {
-        piano_keys[note] = value;
-    }
-}
-
 class Keyboard {
+    constructor(octave_start, octave_end) {
+        this.piano_keys = {};
+        this.octave_start = octave_start || 0
+        this.octave_end = octave_end || 8;
+
+        var keyboard = document.createElement('canvas');
+        document.body.appendChild(keyboard);
+
+        keyboard.width = keyWidth * (this.octave_end - this.octave_start + 1) * 7;
+        keyboard.height = 200;
+
+        var ctx = keyboard.getContext('2d');
+
+        ctx.save();
+        this.ctx = ctx;
+    }
+
+    paint() {
+        // magic values
+        var octaves = 8;
+
+        var blacks = [ 1, 3,     6, 8, 10];
+        var whites = [0, 2, 4, 5, 7, 9, 11];
+        var white_offsets = [0, 1, 3, 4, 5];
+
+        var piano_keys = this.piano_keys
+        var ctx = this.ctx;
+
+        ctx.clearRect(0, 0, keyboard.width, keyboard.height);
+
+        var octave_start = this.octave_start
+        var octave_end = this.octave_end
+        
+        for (var octave = octave_start; octave < octave_end; octave++) {
+            // draw white
+            for (var key = 0; key < 7; key++) {
+                var note = octave * 12 + whites[key];
+                var played = piano_keys[note];
+                
+                drawWhiteNote(ctx, octave - octave_start, key, played)
+            }
+    
+            // draw black
+            for (var key = 0; key < 5; key++) {
+                var note = octave * 12 + blacks[key];
+                var played = piano_keys[note];
+                
+                drawBlackNote(ctx, octave - octave_start, white_offsets[key], played)
+            }
+        }
+    }
+
     toggleNote(note, value) {
-        toggleNote(note, value)
+        if (value === undefined) {
+            this.piano_keys[note] = !this.piano_keys[note];
+        }
+        else {
+            this.piano_keys[note] = value;
+        }
     }
 }
