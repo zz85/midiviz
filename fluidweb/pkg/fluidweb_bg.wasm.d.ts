eff7d293ed9eb87c90b925c05401d1168cf9c62d
Joshua Koo <zz85nus@gmail.com>
2026-01-02 04:32:53 -0800

    build: add wasm-pack output

---
+++ fluidweb/pkg/fluidweb_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_synth_free: (a: number, b: number) => void;
+export const synth_new: (a: number, b: number, c: number) => [number, number, number];
+export const synth_note_on: (a: number, b: number, c: number, d: number) => void;
+export const synth_note_off: (a: number, b: number, c: number) => void;
+export const __wbg_outputdevice_free: (a: number, b: number) => void;
+export const outputdevice_close: (a: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__hced286be6545c1a4: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__h3d0c7cb23895baae: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
